2b3091ded50e7efe8ab5f559bdbeb5076b56da5b
Wolfremium13 <kevincitox1212x@gmail.com>
2021-04-29 12:58:17 +0100

    ✔ exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive

--- src/test/java/CsvFilter/CsvFilter.java
+++ src/test/java/CsvFilter/CsvFilter.java
@@ -1,10 +1,18 @@
 package CsvFilter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class CsvFilter {
     public List<String> filter(List<String> lines) {
-        return lines;
+        List<String> result = new ArrayList<>();
+        result.add(lines.get(0));
+        final String invoice = lines.get(1);
+        final List<String> fields = List.of(invoice.split(","));
+        if (fields.get(4).isBlank() || fields.get(5).isBlank()) {
+            result.add(lines.get(1));
+        }
+        return result;
     }
 }
