a35a3241129c51bbbc56e01ba7bd00dc0810ec9d
Wolfremium13 <kevincitox1212x@gmail.com>
2021-04-29 13:00:57 +0100

    ✔ exclude_lines_with_both_tax_fields_empty_as_one_is_required

--- src/test/java/CsvFilter/CsvFilter.java
+++ src/test/java/CsvFilter/CsvFilter.java
@@ -6,11 +6,12 @@ import java.util.List;
 
 public class CsvFilter {
     public List<String> filter(List<String> lines) {
-        List<String> result = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
         result.add(lines.get(0));
         final String invoice = lines.get(1);
         final List<String> fields = List.of(invoice.split(","));
-        if (fields.get(4).isBlank() || fields.get(5).isBlank()) {
+        if ((fields.get(4).isBlank() || fields.get(5).isBlank()) &&
+                (!(fields.get(4).isBlank() && fields.get(5).isBlank()))) {
             result.add(lines.get(1));
         }
         return result;
