8102e2ca84c84380b172944df9d668b614f8f488
Wolfremium13 <kevincitox1212x@gmail.com>
2021-04-29 12:56:00 +0100

    ❌ exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive

--- src/test/java/CsvFilter/CsvFilterShould.java
+++ src/test/java/CsvFilter/CsvFilterShould.java
@@ -16,4 +16,13 @@ public class CsvFilterShould {
 
         assertThat(result).isEqualTo(List.of(headerLine, invoiceLine));
     }
+    @Test
+    public void exclude_lines_with_both_tax_fields_populated_as_they_are_exclusive() {
+        final String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+        final String invoiceLine = "1,02/05/2019,100,810,19,8,ACER Laptop,B76430134,";
+
+        List<String> result = new CsvFilter().filter(List.of(headerLine, invoiceLine));
+
+        assertThat(result).isEqualTo(List.of(headerLine));
+    }
 }
