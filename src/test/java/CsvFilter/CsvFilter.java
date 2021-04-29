28fe30d2688fd40967870358d6171472fac71606
Wolfremium13 <kevincitox1212x@gmail.com>
2021-04-29 12:52:58 +0100

    ❌ allow_for_correct_lines_only

---
+++ src/test/java/CsvFilter/CsvFilter.java
@@ -0,0 +1,10 @@
+package CsvFilter;
+
+import java.util.List;
+
+
+public class CsvFilter {
+    public List<String> filter(List<String> lines) {
+        return List.of();
+    }
+}
---
+++ src/test/java/CsvFilter/CsvFilterShould.java
@@ -0,0 +1,19 @@
+package CsvFilter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CsvFilterShould {
+    @Test
+    public void allow_for_correct_lines_only() {
+        final String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+        final String invoiceLine = "1,02/05/2019,100,810,19,,ACER Laptop,B76430134,";
+
+        List<String> result = new CsvFilter().filter(List.of(headerLine, invoiceLine));
+
+        assertThat(result).isEqualTo(List.of(headerLine, invoiceLine));
+    }
+}
